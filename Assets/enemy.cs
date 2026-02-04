ad7d0879d2134a8de8086f9f66bbd9ce433b2dd8
Excellentded <simonhedebacke@outlook.com>
2026-02-04 11:09:34 +0100

    la till enemy
    test

---
+++ Assets/enemy.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 1f;
+    private Rigidbody2D rb;
+    private UnityEngine.Vector2 movement;
+    private SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Vector3 direction = player.position - transform.position; //melee_enemy jämfört med player
+        rb.linearVelocityX = direction.x;
+        direction.Normalize();
+        movement = direction;
+        if (rb.linearVelocity.x > 0.01f)
+            sr.flipX = false;
+        else if (rb.linearVelocity.x < -0.01f)
+            sr.flipX = true;
+
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+    void moveCharacter(UnityEngine.Vector2 direction)
+    {
+        rb.MovePosition((UnityEngine.Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
