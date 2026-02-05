15851227d8c834d97f024faaaf13ca1c4e44253a
Excellentded <simonhedebacke@outlook.com>
2026-02-05 10:20:04 +0100

    Slime targetting fix
    kom ihåg att spawna slimes samtidigt som gubbe eller efter annars funkar de inte

--- Assets/scripts/enemy/enemy.cs
+++ Assets/scripts/enemy/enemy.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform player;
+    //public Transform player;
     public float moveSpeed = 1f;
     private Rigidbody2D rb;
     private UnityEngine.Vector2 movement;
     private SpriteRenderer sr;
+    GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
         rb = this.GetComponent<Rigidbody2D>();
         sr = this.GetComponent<SpriteRenderer>();
     }
@@ -20,7 +23,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UnityEngine.Vector3 direction = player.position - transform.position; //melee_enemy jämfört med player
+        UnityEngine.Vector3 direction = player.transform.position - transform.position; //melee_enemy jämfört med player
         rb.linearVelocityX = direction.x;
         direction.Normalize();
         movement = direction;
